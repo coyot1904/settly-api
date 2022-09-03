51729b3af148eda94fb54ba9caacb6b9601a5ef0
coyot1904 <coyot1904@gmail.com>
2022-09-03 18:19:14 +0300

    modify test

--- tests/Feature/ClientTest.php
+++ tests/Feature/ClientTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class ClientTest extends TestCase
@@ -21,6 +23,26 @@ class ClientTest extends TestCase
         $response = $this->withHeaders([
             'Authorization' => 'Bearer 46|bZHo0GovhYD5vBQIl2m4E0Nn5gPZSuerlieFGrmr',
         ])->get('/api/clinet/single/1');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_add_client()
+    {
+        Storage::fake('local');
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer 46|bZHo0GovhYD5vBQIl2m4E0Nn5gPZSuerlieFGrmr',
+        ])->post('/api/clinet/add/' , ['name' => 'test' , 'email' => 'email@mail.com' , 'image' => UploadedFile::fake()->image('avatar.jpg')]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_edit_client()
+    {
+        Storage::fake('local');
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer 46|bZHo0GovhYD5vBQIl2m4E0Nn5gPZSuerlieFGrmr',
+        ])->post('/api/clinet/edit/' , ['name' => 'test' , 'email' => 'email@mail.com' , 'image' => UploadedFile::fake()->image('avatar.jpg') , 'id' => 1]);
 
         $response->assertStatus(200);
     }
